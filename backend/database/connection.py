bd862144a599ed7988a9db70e0b1de69ebd51643
mago <magomedovmarif2@gmail.com>
2024-08-31 20:52:09 +0300

    fix db

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -11,3 +11,4 @@ engine = create_async_engine(url)
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.drop_all)
